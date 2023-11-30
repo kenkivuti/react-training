7888225cd67ff8eff4097e53b2f16a9fa74c00de
ken kivuti <kivutiken4@gmail.com>
2023-11-30 17:58:35 +0300

    begginer

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import Message from "./message";
+
+function App () {
+  //  <Message/> this is called the self-closing syntax
+  return <div><Message/></div>;
+}
+
+export default App ;
---
+++ src/message.tsx
@@ -0,0 +1,12 @@
+
+// create a function and name the function using PascalCasing
+function Message (){
+    // JSX : javascript XML meaning the code will be compiled to javascript
+    let name = 'peace';
+    if (name)
+      return <h1>Hello {name}</h1> 
+    else
+     return <h1>Hello World</h1>  
+}
+
+export default Message;
