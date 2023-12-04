564d6f147e2dee246f314e39ed106cf851e7c4ae
ken kivuti <kivutiken4@gmail.com>
2023-12-04 22:03:27 +0300

    creating list group

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,16 +1,34 @@
 import { Fragment } from "react";
 
 function ListGroup() {
-  const items = ["New York", "Nairobi", "Canada", "Comptown", "Tokyo"];
+  let items = ["New York", "Nairobi", "Canada", "Comptown", "Tokyo"];
+
+  let SelectedIndex = 0
+  // you can also add an if statement or functions
+
+  {
+    /* so as not to repeat yourself you can write */
+  }
+
+  function GetMessage() {
+    return items.length == 0 && <p>No item</p>;
+  }
 
   return (
     <Fragment>
       <h1>list</h1>
+      {GetMessage()}
       <ul className="list-group">
         {/* in  jsx we do not use for loop so we use mapping method instead */}
         {/* we use {} since in jsx we can only use html elements or react components */}
         {items.map((item) => (
-          <li key={item}>{item}</li>
+          <li
+            className="list-group-item active"
+            key={item}
+            onClick={() => console.log(item)}
+          >
+            {item}
+          </li>
         ))}
       </ul>
     </Fragment>
