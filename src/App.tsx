b42bcf4e11a4cbaaab0cb0fdac8308283bc27b51
ken kivuti <kivutiken4@gmail.com>
2023-12-05 22:52:57 +0300

    state and props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import ListGroup from "./components/ListGroup";
-
+const handleSelectItem =(item: string) => {
+  console.log(item)
+}
 function App () {
+  let items = ["New York", "Nairobi", "Canada", "Comptown", "Tokyo"];
   //  <Message/> this is called the self-closing syntax
-  return <div><ListGroup/></div>;
+  return <div><ListGroup items ={items} heading="Cities" onSelectItem={handleSelectItem}/></div>;
 }
 
 export default App ;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,9 +1,22 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+// items []; heading string
+interface Props {
+  items : string[];
+  heading : string;
 
-function ListGroup() {
-  let items = ["New York", "Nairobi", "Canada", "Comptown", "Tokyo"];
+  onSelectItem: (item: string) => void
+}
 
-  let SelectedIndex = 0
+
+function ListGroup({items, heading,onSelectItem} : Props) {
+  
+
+  
+  // this is alled a hook : it allows to tap in into built in features in react
+  // using state hook means we are telling react that it will have data or state that will change over time
+   let  [SelectedIndex , setSelectedIndex]= useState(-1);
+   
+
   // you can also add an if statement or functions
 
   {
@@ -16,16 +29,16 @@ function ListGroup() {
 
   return (
     <Fragment>
-      <h1>list</h1>
+      <h1>{heading}</h1>
       {GetMessage()}
       <ul className="list-group">
         {/* in  jsx we do not use for loop so we use mapping method instead */}
         {/* we use {} since in jsx we can only use html elements or react components */}
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
-            className="list-group-item active"
+            className={ SelectedIndex == index ? "list-group-item active" : "list-group-item "}
             key={item}
-            onClick={() => console.log(item)}
+            onClick={() => {setSelectedIndex(index); onSelectItem(item)}}
           >
             {item}
           </li>
